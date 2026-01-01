556d74565359f772d91c5bdd709f52b7a8dd9231
bilalmaq01 <bilalmaq01@gmail.com>
2026-01-01 17:37:18 -0600

    Pops up a gui that is just a black screen right now

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    infiniteloop = 0
+    while(infiniteloop==0):
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
