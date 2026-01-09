da889e6584d6b18298c50a6516555b5011efb357
bilalmaq01 <bilalmaq01@gmail.com>
2026-01-09 12:10:03 -0600

    Asteroids now split when shot into two smaller asteroids and dissapeared once an asteroid thats smaller or equal to smallest radius allowed is shot

--- asteroids.py
+++ asteroids.py
@@ -1,6 +1,9 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_state, log_event
+import random
+
 
 class Asteroid(CircleShape):
     def __init__(self,x,y,radius):
@@ -11,5 +14,25 @@ class Asteroid(CircleShape):
 
     def update(self,dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            new_angle = random.uniform(20,50)
+            asteroid1_vector = self.velocity.rotate(new_angle)
+            asteroid2_vector = self.velocity.rotate(-new_angle)
+            new_radius = self.radius -ASTEROID_MIN_RADIUS
+            new_asteroid1 = Asteroid(self.position.x,self.position.y,new_radius)
+            new_asteroid1.velocity = asteroid1_vector * 1.2
+            new_asteroid2 = Asteroid(self.position.x,self.position.y,new_radius)
+            new_asteroid2.velocity = asteroid2_vector * 1.2
+
+
+        
+
+
 
 
