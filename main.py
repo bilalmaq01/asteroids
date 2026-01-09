eb0b659ef58b952701b5fcd364b94857f1363d3c
bilalmaq01 <bilalmaq01@gmail.com>
2026-01-09 10:59:18 -0600

    Have asteroids spawn in now, When player collides with asteroid game ends.When a shot hits an asteroid the asteroid and shot dissapear.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT
-from logger import log_state
+import sys
+from constants import *
+from logger import log_state, log_event
+from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+from shots import Shot 
 
 def main():
     pygame.init()
@@ -9,13 +14,50 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+    fpsClock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots,updatable,drawable)
+    Player.containers = (updatable,drawable)
+
+    ship = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+    field = AsteroidField() 
+
+#game loop 
     infiniteloop = 0
     while(infiniteloop==0):
         log_state()
+        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if ship.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        timepassed = fpsClock.tick(60)
+        dt = timepassed / 1000
+
 if __name__ == "__main__":
     main()
